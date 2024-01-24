d1e36326948a4abec64e07620d14664be4756e27
kazirahman <kazif2122r@gmail.com>
2024-01-24 02:11:48 -0500

    Playing around with vector databses

--- api-logic.py
+++ api-logic.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import PyPDF2
 import numpy as np
@@ -7,7 +8,7 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased-v2')
 
 from qdrant_client import models,QdrantClient
-
+start_time = time.time()
 def walk_pdf_dir(pathpdfs):
     list_of_pdfs = {}
     for (dirpath, dirnames, filenames) in os.walk(pathpdfs):
@@ -28,7 +29,7 @@ def read_pdf(listofpfds):
     return pdf_content
 
 
-def create_vector_database(collection_name,store = None):
+def create_vector_database(text_search,collection_name,pdf_dict_data,store=None):
 
     if store == None:
         storage = QdrantClient(":memory:")
@@ -44,7 +45,27 @@ def create_vector_database(collection_name,store = None):
         )
     )
 
-def generate(pdf_path,db ='no'):
+    storage.upload_records(
+        collection_name=collection_name,
+        records=[
+            models.Record(
+                id=idx,
+                vector=model.encode(doc[pdf_dict_data.items()]).tolist(),
+                payload=doc
+            ) for idx, doc in enumerate(pdf_dict_data)
+        ]
+    )
+
+
+    hits = storage.search(
+        collection_name=collection_name,
+        query_vector=model.encode(text_search).tolist(),
+        limit=2
+    )
+    for hit in hits:
+        print(hit.payload, "score:", hit.score)
+
+def generate(text_search,pdf_path,db ='no'):
     embdedded_pdf = {}
     pdf_data = walk_pdf_dir(pdf_path)
     for key,value in pdf_data.items():
@@ -53,7 +74,7 @@ def generate(pdf_path,db ='no'):
         embdedded_pdf[key] = embeddings_list
 
     if db == 'yes':
-       create_vector_database('pdf')
+       create_vector_database(text_search,'pdf',pdf_data,)
        print('Files embedded and stored in a vector database')
     else:
 
@@ -63,4 +84,6 @@ def generate(pdf_path,db ='no'):
 
     return None
 
-
+generate('experience','/Users/kazirahman/PycharmProjects/PDFAPI/testing_data',db='yes')
+end_time = time.time()
+print("Execution time:", end_time - start_time, "seconds")
